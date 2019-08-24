8ae2880cd5107f53dc4b21cf552962881bf96e9b
mukesh-mehta <mukesh.iitr18@gmail.com>
2019-08-24 14:19:11 +0530

    changed rle2mask fns

--- src/utils.py
+++ src/utils.py
@@ -32,3 +32,10 @@ def predict(X, threshold):
     X_p = np.copy(X)
     preds = (X_p > threshold).astype('uint8')
     return preds
+
+def epoch_log(phase, epoch, epoch_loss, meter, start):
+    '''logging the metrics at the end of an epoch'''
+    dices, iou = meter.get_metrics()
+    dice, dice_neg, dice_pos = dices
+    print("Loss: %0.4f | dice: %0.4f | dice_neg: %0.4f | dice_pos: %0.4f | IoU: %0.4f" % (epoch_loss, dice, dice_neg, dice_pos, iou))
+    return dice, iou
