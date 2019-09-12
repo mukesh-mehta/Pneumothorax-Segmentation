4d69277ccfc83d141b9c354fabb1783b91e68101
Mukesh Kr Mehta <mukesh.iitr18@gmail.com>
2019-09-12 18:42:38 +0530

    Revert "added new loss fns"
    This reverts commit 3159c7a9341c2d12b286668147216d8f30f676d5.

--- src/losses.py
+++ src/losses.py
@@ -1,27 +1,6 @@
-import os
-import cv2
-import pdb
-import time
-import warnings
-
-import config
-
-import random
-import numpy as np
-import pandas as pd
-from tqdm import tqdm_notebook as tqdm
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from sklearn.model_selection import StratifiedKFold
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
-import torch.optim as optim
-import torch.backends.cudnn as cudnn
-from torch.utils.data import DataLoader, Dataset, sampler
-from matplotlib import pyplot as plt
-from albumentations import (HorizontalFlip, ShiftScaleRotate, Normalize, Resize, Compose, GaussNoise)
-from albumentations.torch import ToTensor
-import segmentation_models_pytorch as smp
+import torch.nn.functional as F
 
 def dice_loss(input, target):
     input = torch.sigmoid(input)
